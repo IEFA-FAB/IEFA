a2be4a3bfe72d7a78ec39cd9aefbc05f9c737044
userNanni <usuarionanni@gmail.com>
2025-11-05 10:45:37 -0300

    delete out

--- out/build/client/assets/RanchoUtils-CDHCvR11.js
+++
@@ -1,6 +0,0 @@
-import{c as n}from"./createLucideIcon-CwMMISn7.js";/**
- * @license lucide-react v0.537.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const s=[["path",{d:"M3 2v7c0 1.1.9 2 2 2h4a2 2 0 0 0 2-2V2",key:"cjf0a3"}],["path",{d:"M7 2v20",key:"1473qp"}],["path",{d:"M21 15V2a5 5 0 0 0-5 5v6c0 1.1.9 2 2 2h3Zm0 0v7",key:"j28e5"}]],o=n("utensils",s),r=()=>({cafe:!1,almoco:!1,janta:!1,ceia:!1}),i=e=>new Date(e+"T00:00:00").toLocaleDateString("pt-BR",{day:"2-digit",month:"2-digit",year:"numeric"}),d=e=>new Date(e+"T00:00:00").toLocaleDateString("pt-BR",{weekday:"long"}),D=(e,t=2)=>{const a=new Date;return a.setDate(a.getDate()+t),new Date(e+"T00:00:00")<=a};export{o as U,r as c,i as f,d as g,D as i};
--- out/build/client/assets/label-DlsFJCEv.js
+++
@@ -1,6 +0,0 @@
-import{c as i}from"./createLucideIcon-CwMMISn7.js";import{j as a}from"./jsx-runtime-D_zvdyIk.js";import{a as l}from"./chunk-OIYGIGL5-BjQr_l_M.js";import{P as s}from"./index-B2a0W2do.js";import{c as n}from"./utils-BEHD0UYf.js";/**
- * @license lucide-react v0.537.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const c=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["line",{x1:"12",x2:"12",y1:"8",y2:"12",key:"1pkeuh"}],["line",{x1:"12",x2:"12.01",y1:"16",y2:"16",key:"4dfq90"}]],g=i("circle-alert",c);var d="Label",o=l.forwardRef((t,r)=>a.jsx(s.label,{...t,ref:r,onMouseDown:e=>{e.target.closest("button, input, select, textarea")||(t.onMouseDown?.(e),!e.defaultPrevented&&e.detail>1&&e.preventDefault())}}));o.displayName=d;var m=o;function L({className:t,...r}){return a.jsx(m,{"data-slot":"label",className:n("flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",t),...r})}export{g as C,L};
