b606bfd9c66f0ca557e08fb8c234dc3e59a6a2d6
userNanni <usuarionanni@gmail.com>
2025-12-19 12:09:56 -0300

    forgot password fixed

--- packages/ui/src/components/ui/sidebar.tsx
+++ packages/ui/src/components/ui/sidebar.tsx
@@ -201,7 +201,7 @@ function Sidebar({
 	...props
 }: ComponentProps<"div"> & {
 	side?: "left" | "right";
-	variant?: "sidebar" | "floating" | "inset";
+	variant?: "sidebar" | "floating" | "floating";
 	collapsible?: "offcanvas" | "icon" | "none";
 }) {
 	const { isMobile, state, openMobile, setOpenMobile } = useSidebar();
@@ -262,7 +262,7 @@ function Sidebar({
 					"relative w-(--sidebar-width) bg-transparent transition-[width] duration-200 ease-linear",
 					"group-data-[collapsible=offcanvas]:w-0",
 					"group-data-[side=right]:rotate-180",
-					variant === "floating" || variant === "inset"
+					variant === "floating" || variant === "floating"
 						? "group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4)))]"
 						: "group-data-[collapsible=icon]:w-(--sidebar-width-icon)",
 				)}
@@ -275,7 +275,7 @@ function Sidebar({
 						? "left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]"
 						: "right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]",
 					// Adjust the padding for floating and inset variants.
-					variant === "floating" || variant === "inset"
+					variant === "floating" || variant === "floating"
 						? "p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4))+2px)]"
 						: "group-data-[collapsible=icon]:w-(--sidebar-width-icon) group-data-[side=left]:border-r group-data-[side=right]:border-l",
 					className,
